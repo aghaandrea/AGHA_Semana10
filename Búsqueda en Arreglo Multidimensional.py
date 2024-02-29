ee98fd7dab1c7a828ac2be0ab0e78fa61d5fd69b
andrea hernandez <ag.hernandeza@uea.edu.ec>
2024-02-28 19:11:11 -0500

    semana 11 deber

--- Búsqueda en Arreglo Multidimensional.py
+++ Búsqueda en Arreglo Multidimensional.py
@@ -7,21 +7,21 @@ def buscar_valor(matriz, valor):
             return False, None
 
 # Ejemplo de uso
-    def main():
-        mi_matriz=[
+def main():
+    mi_matriz=[
      [1,2,3],
      [4,5,6],
      [7,8,9]
      ]
 
 
-valor_a_buscar = 3
-encontrado, posicion = buscar_valor(mi_matriz, valor_a_buscar)
+    valor_a_buscar = 3
+    encontrado, posicion = buscar_valor(mi_matriz, valor_a_buscar)
 
-if encontrado:
-    print(f"El valor {valor_a_buscar} se encuentra en la posicion {posicion}")
-else:
-    print(f"El valor {valor_a_buscar} no se encuentra en la matriz.")
+    if encontrado:
+        print(f"El valor {valor_a_buscar} se encuentra en la posicion {posicion}")
+    else:
+        print(f"El valor {valor_a_buscar} no se encuentra en la matriz.")
 
 if __name__=="main__":
     main()
