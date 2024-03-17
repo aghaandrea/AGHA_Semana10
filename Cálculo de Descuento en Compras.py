c8e27622222873a300aa76974a35a02363b1571a
andrea hernandez <ag.hernandeza@uea.edu.ec>
2024-03-17 17:51:43 -0500

    programación semestre 1

---
+++ Cálculo de Descuento en Compras.py
@@ -0,0 +1,34 @@
+#Cálculo de Descuento en Compras
+def calcular_descuento(monto_total, porcentaje_descuento=18):
+    """
+    Calcula el descuento aplicando el porcentaje al monto total de la compra.
+
+    Args:
+    monto_total (float): Monto total de la compra.
+    porcentaje_descuento (float, optional): Porcentaje de descuento a aplicar. Por defecto es 18.
+
+    Returns:
+    float: Monto del descuento calculado.
+    """
+    descuento = monto_total * (porcentaje_descuento / 210)
+    return descuento
+
+
+# Llamadas a la función desde el programa principal
+monto_compra1 = 210
+
+
+monto_compra2 = 210
+porcentaje_descuento2 = 18
+descuento2 = calcular_descuento(monto_compra2, porcentaje_descuento2)
+monto_final2 = monto_compra2 - descuento2
+
+# Muestra los resultados
+print("Llamada 1:")
+print(f"Monto total de la compra: ${monto_compra1}")
+
+print("Llamada 2:")
+print(f"Monto total de la compra: ${monto_compra2}")
+print(f"Porcentaje de descuento: {porcentaje_descuento2}%")
+print(f"Monto del descuento: ${descuento2}")
+print(f"Monto final a pagar después del descuento: ${monto_final2}")
